fa96cb48e4a7d4b423adf6d7f4b4a2748848cd1d
erenoguzyesil <erenoguzyesil29@gmail.com>
2022-09-02 02:48:11 +0300

    Added new features.

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const selects = {
     connective: document.getElementById('logical-connective')
 }
 
+const radioButtons = {
+    word: document.getElementById('option-word'),
+    number: document.getElementById('option-number'),
+    abbreviation: document.getElementById('option-abbreviation')
+}
+
 const truthTable = document.getElementById('truth-table');
 
 const operations = {
@@ -24,10 +30,16 @@ const operations = {
 }
 
 function capitalize(x) {
+    let truthValues = [];
+
+    if (radioButtons.word.checked) truthValues = ['True', 'False'];
+    if (radioButtons.abbreviation.checked) truthValues = ['T', 'F'];
+    if (radioButtons.number.checked) truthValues = ['1', '0'];
+
     if (x == true) {
-        return 'True';
-    } else if (x == false) {
-        return 'False';
+        return truthValues[0];
+    } else {
+        return truthValues[1];
     }
 }
 
@@ -112,32 +124,41 @@ function makeTable(connective, s1negated, s2negated) {
     return result;
 }
 
-Object.values(selects).forEach(e => {
-    addEventListener('change', () => {
-        let connective = selects.connective.value;
+function run() {
+    Object.values(selects).forEach(e => {
+        addEventListener('change', () => {
+            let connective = selects.connective.value;
 
-        let pNegated = false;
-        let qNegated = false;
+            let pNegated = false;
+            let qNegated = false;
 
-        if (selects.statement1.value == 'negated') { pNegated = true; }
-        if (selects.statement2.value == 'negated') { qNegated = true; }
+            if (selects.statement1.value == 'negated') { pNegated = true; }
+            if (selects.statement2.value == 'negated') { qNegated = true; }
 
-        truthTable.innerHTML = makeTable(connective, pNegated, qNegated);
+            truthTable.innerHTML = makeTable(connective, pNegated, qNegated);
 
-        if (truthTable.offsetWidth >= document.body.offsetWidth) {
-            // If the table doesn't fit the screen:
+            if (truthTable.offsetWidth >= document.body.offsetWidth) {
+                // If the table doesn't fit the screen:
 
-            document.querySelectorAll('#truth-table td').forEach((e) => {
-                e.style.padding = '5px 10px';
-            });
+                document.querySelectorAll('#truth-table td').forEach((e) => {
+                    e.style.padding = '5px 10px';
+                });
 
-            document.querySelectorAll('#truth-table th').forEach((e) => {
-                e.style.padding = '5px';
-            });
-        }
+                document.querySelectorAll('#truth-table th').forEach((e) => {
+                    e.style.padding = '5px';
+                });
+            }
+        });
+    });
+}
+
+Object.values(radioButtons).forEach((e) => {
+    e.addEventListener('change', () => {
+        run();
     });
 });
 
 window.addEventListener('load', () => {
+    run();
     truthTable.innerHTML = makeTable('and', false, false);
 });
